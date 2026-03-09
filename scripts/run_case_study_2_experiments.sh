f1f82f85dea57efdbd3f26a6a0bb8f82e765bfc3
PawPuk <pawel.piotr.pukowski@gmail.com>
2026-03-09 15:36:35 +0000

    Added slurm scripts for reruning all of the experiments conducted in the paper.

--- scripts/run_case_study_2_experiments.sh
+++ scripts/run_case_study_2_experiments.sh
@@ -10,10 +10,6 @@ echo "Training models on balanced dataset (the baseline)..."
 python -m src.experiments.train_baseline_models --dataset_name CIFAR100
 
 echo "Training models on resampled full dataset (case study 2)..."
-# python -m src.experiments.case_study_2 --dataset_name CIFAR100 --oversampling random --undersampling easy --alpha 2
-# python -m src.experiments.case_study_2 --dataset_name CIFAR100 --oversampling SMOTE --undersampling easy --alpha 2
-# python -m src.experiments.case_study_2 --dataset_name CIFAR100 --oversampling rEDM --undersampling easy --alpha 2
-# python -m src.experiments.case_study_2 --dataset_name CIFAR100 --oversampling aEDM --undersampling easy --alpha 2
 python -m src.experiments.case_study_2 --dataset_name CIFAR100 --oversampling hEDM --undersampling easy --alpha 2
 
 echo ""
---
+++ scripts/slurm/visualize_case_study_1_results.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --mem=16G
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=36:00:00
+
+# Load the modules required by our program
+module load Anaconda3/2022.05
+module load CUDA/10.2.89-GCC-8.3.0
+source activate pytorch
+
+dataset_name=$1
+
+python3 src.analysis.visualize_case_study_1_results.py --dataset_name "$dataset_name"
